4aeb71cf34d72d407463e85c7a7acca08342b7f1
a95500101-hash <a95500101@gmail.com>
2025-11-18 21:57:53 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
-document.getElementById("buyButton").addEventListener("click", function () {
-    window.location.href = "https://buy.stripe.com/test_dRmdR1czdaO9eGZ4YNaEE00";
+const container = document.getElementById("products");
+
+products.forEach(p => {
+    container.innerHTML += `
+        <div class="product-card">
+            <img src="${p.image}">
+            <h3>${p.name}</h3>
+            <p>${p.price} ډالر</p>
+            <button onclick="window.location.href='${p.link}'">خریداری</button>
+        </div>
+    `;
 });
