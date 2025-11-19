6e23dcee1cea87067c9e4e7d5f94c507ad51ae94
a95500101-hash <a95500101@gmail.com>
2025-11-18 22:00:07 -0500

    Create products.js

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        name: "موبایل چارجر",
+        price: 5,
+        image: "https://via.placeholder.com/200",
+        link: "https://buy.stripe.com/test_dRmdR1czdaO9eGZ4YNaEE00"
+    },
+    {
+        name: "وائرلس کیبورډ",
+        price: 20,
+        image: "https://via.placeholder.com/200",
+        link: "https://buy.stripe.com/test_dRmdR1czdaO9eGZ4YNaEE00"
+    },
+    {
+        name: "ایر بډز",
+        price: 15,
+        image: "https://via.placeholder.com/200",
+        link: "https://buy.stripe.com/test_dRmdR1czdaO9eGZ4YNaEE00"
+    }
+];
