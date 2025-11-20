9e3eaf855b5cc80a05e0fc71ecbc5f6630d11634
a95500101-hash <a95500101@gmail.com>
2025-11-19 22:34:05 -0500

    Update products.js

--- products.js
+++ products.js
@@ -1,20 +1,7 @@
+// مثال: د ګڼ شمېر محصولاتو لپاره دا فایل وکاروه او هر محصول د array په توګه اضافه کړه
 const products = [
-    {
-        name: "موبایل چارجر",
-        price: 5,
-        image: "https://via.placeholder.com/200",
-        link: "https://buy.stripe.com/test_dRmdR1czdaO9eGZ4YNaEE00"
-    },
-    {
-        name: "وائرلس کیبورډ",
-        price: 20,
-        image: "https://via.placeholder.com/200",
-        link: "https://buy.stripe.com/test_dRmdR1czdaO9eGZ4YNaEE00"
-    },
-    {
-        name: "ایر بډز",
-        price: 15,
-        image: "https://via.placeholder.com/200",
-        link: "https://buy.stripe.com/test_dRmdR1czdaO9eGZ4YNaEE00"
-    }
+  { id: "p1", name: "موبایل چارجر", price: 5, image: "https://via.placeholder.com/300" },
+  { id: "p2", name: "وائرلس کیبورډ", price: 20, image: "https://via.placeholder.com/300" },
+  { id: "p3", name: "ایر بډز", price: 15, image: "https://via.placeholder.com/300" }
+  // ته کولی شې سلګونه item دلته اضافه کړې
 ];
